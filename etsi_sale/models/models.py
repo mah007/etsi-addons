27861342b911aaa1950bfe73988a00ad72db2ab4
Giezel Esteves <giezelesteves23@gmail.com>
2017-08-01 15:09:08 +0800

    modify etsi_sale model.py

--- etsi_sale/models/models.py
+++ etsi_sale/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class etsi_sale(models.Model):
-#     _name = 'etsi_sale.etsi_sale'
+class etsi_sale(models.Model):
+    _name = 'etsi_sale.etsi_sale'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
