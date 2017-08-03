c82a9b66038fcc07d57c16ce0d7f5ad332d98bac
Deb <adevadeborah@gmail.com>
2017-08-03 16:04:38 +0800

    separate annual report wizard views

--- etsi_payroll/models/hr_annual_tax_report.py
+++ etsi_payroll/models/hr_annual_tax_report.py
@@ -3,6 +3,8 @@ from odoo import fields,api,models
 class AnnualTaxReport(models.Model):
     _name = 'hr.payroll.annual.tax.report'
 
-    empl_id = fields.Many2one('res.partner', string='Employee')
-    year = fields.Selection([(num, str(num)) for num in range(2000, (fields.datetime.now().year) + 1)], 'Year')
+    annual_company_id = fields.Many2one('res.partner', string='Company')
+    year_selection = fields.Selection([(num, str(num)) for num in range(2000, (fields.datetime.now().year) + 1)], 'Year')
+
+    # def print_payroll_register(self,data):
 
