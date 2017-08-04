b3f5ca40cc95f3a2b89e1c46a2fa0e44123d851a
Dayle <daylevillanda@gmail.com>
2017-08-04 09:40:23 +0800

    create annual tax report

--- etsi_payroll/models/hr_annual_tax_report.py
+++ etsi_payroll/models/hr_annual_tax_report.py
@@ -6,5 +6,11 @@ class AnnualTaxReport(models.Model):
     annual_company_id = fields.Many2one('res.partner', string='Company')
     year_selection = fields.Selection([(num, str(num)) for num in range(2000, (fields.datetime.now().year) + 1)], 'Year')
 
-    # def print_payroll_register(self,data):
+    def print_annual_tax_report(self,data):
+        data = {}
+
+        data['ids'] = self.env.context.get('active_ids',[])
+        data['model'] = self.env.context.get('active_model','ir.ui.menu')
+        data['form'] = self.read(['annual_company_id'])[0]
+        return self.env['report'].sudo().get_action(self, 'etsi_payroll.report_annual_tax_template', data=data)
 
