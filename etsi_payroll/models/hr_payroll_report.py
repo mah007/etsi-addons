8f061b3863fa306942ca04ba4f5eea808615ae62
elishah19 <elishah.garcia019@gmail.com>
2017-08-04 10:08:02 +0800

    Merge branch 'master' of https://github.com/EVANSTECHS/etsi-addons
    # Conflicts:
    #	etsi_payroll/models/__init__.py

--- etsi_payroll/models/hr_payroll_report.py
+++ etsi_payroll/models/hr_payroll_report.py
@@ -10,8 +10,21 @@ def get_years():
 class EmployeePayrollReport(models.Model):
     _name = 'hr.payroll.report'
 
-    company = fields.Many2one('res.partner', string="Company", domain=[('is_company','=', True)])
-    # year_date = fields.Datetime(string="Year")
-    # year_date = fields.Date(default=lambda *a: time.strftime('%Y-12-31'))
-
+    com_id= fields.Many2one('res.partner', string="Company", domain=[('is_company','=', True)])
     year_date = fields.Selection(get_years(), string='Year',)
+    # year_date = fields.Selection([(num, str(num)) for num in range(2000, (fields.datetime.now().year) + 1)], 'Year')
+
+    @api.multi
+    def print_company_annual_tax(self, data):
+        data = {}
+
+        data['ids'] = self.env.context.get('active_ids', [])
+        data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
+        data['form'] = self.read(['com_id', ])[0]
+        print 'data', data
+
+        return self.env['report'].sudo().get_action(self, 'etsi_payroll.report_annual_tax_report', data=data)
+
+        # data = {}
+        # data['form'] = self.read(['com_id',])[0]
+        # return self.env['report'].get_action(self, 'etsi_payroll.report_annual_tax_report', data=data)
