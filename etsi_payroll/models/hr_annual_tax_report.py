1215edb2fbd1dcd2d6d507c13b83f26fae4a8f6b
Deb <adevadeborah@gmail.com>
2017-08-04 13:36:29 +0800

    display employee

--- etsi_payroll/models/hr_annual_tax_report.py
+++ etsi_payroll/models/hr_annual_tax_report.py
@@ -4,18 +4,19 @@ from datetime import datetime, timedelta
 class AnnualTaxReport(models.Model):
     _name = 'hr.payroll.annual.tax.report'
 
-    annual_company_id = fields.Many2one('res.partner', string='Company')
     def _get_years(self):
         this_year = datetime.today().year
 
         results = [(str(x), str(x)) for x in range(this_year - 40, this_year + 10)]
         return results
 
+    annual_company_id = fields.Many2one('res.partner', string='Company')
     year_selection = fields.Selection(_get_years, string="Select Year")
 
 
 
-    def print_annual_tax_report(self,data):
+
+    def print_annual_tax_report(self, data):
         data = {}
 
         data['ids'] = self.env.context.get('active_ids',[])
