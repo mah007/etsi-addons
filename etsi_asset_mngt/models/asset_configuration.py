d1d9bed5f27b60fd937c9a66f8eb329c18f47f7e
Lawrence Cute <lawrencelanzaderas020497@gmail.com>
2017-08-18 14:33:35 +0800

    modify in etsi_asset_mngt

--- etsi_asset_mngt/models/asset_configuration.py
+++ etsi_asset_mngt/models/asset_configuration.py
@@ -4,11 +4,11 @@ class AssetConfiguration(models.Model):
     _name = 'asset.config'
 
     serial_num = fields.Char(string="Serial #")
-    asset_name = fields.Char(string="Asset")
+    name = fields.Char(string="Asset")
     asset_model = fields.Char(string="Model")
     year = fields.Date(string="Year")
     asset_condition = fields.Selection([('1', 'BRAND NEW'),
                                         ('2', 'USED'),
                                         ('3', 'DAMAGED')],
                                        string="Asset Condition")
-    quantity = fields.Float(string="Quantity")
+    # quantity = fields.Float(string="Quantity")
--- etsi_asset_mngt/models/asset_handover.py
+++ etsi_asset_mngt/models/asset_handover.py
@@ -1,17 +1,57 @@
 from odoo import api, fields, models
 from datetime import datetime
+from odoo.exceptions import ValidationError
 
 class AssetManagementHandover (models.Model):
     _name = 'asset.management.handover'
 
     company_id = fields.Many2one ('res.partner', string = "Company")
     emp_id = fields.Many2one ('hr.employee', string = "Employee")
-    emp_email = fields.Char (string = "Email", readonly = True)
-    source_loc = fields.Char (string = "Source Location", readonly = True)
-    destination_loc = fields.Char (string = "Destination Location", readonly = True)
+    emp_email = fields.Char (string = "Email", readonly = True, related = 'emp_id.user_id.login')
+    source_loc = fields.Char (string = "Source Location", readonly = True, related = 'company_id.name')
+    destination_loc = fields.Char (string = "Destination Location", readonly = True, related = 'emp_id.name')
     date = fields.Date (string = "Date", default = lambda *a: datetime.today())
-    transfer_type = fields.Char (string = "Transfer type")
-    it_engineer = fields.Char (string = "IT Engineer")
-    processed_by = fields.Char (string = "Processed by")
-    internal_trans = fields.Char (string = "Internal Transfer")
-    # samplechanges
+    transfer_type = fields.Char (string = "Transfer type", default = "Asset Handover", readonly = True)
+    it_engineer_id = fields.Many2one ('res.users', string = "IT Engineer", readonly = True, default=lambda self: self.env.uid)
+    processed_by = fields.Many2one ('hr.employee', string = "Processed by", readonly = True)
+    internal_trans = fields.Char (string = "Internal Transfer", readonly = True)
+
+    state = fields.Selection ([
+        ('draft', "Draft"),
+        ('transfer', "Transferred"),
+        ('cancel', "Cancelled"),
+    ], string = "State", default = 'draft')
+
+    @api.multi
+    def button_print(self):
+        print 'print'
+        raise ValidationError ("Mark inserted print function here")
+
+    @api.multi
+    def button_transfer(self):
+        self.state = 'transfer'
+        self.processed_by = self.env['hr.employee'].browse(self.env.uid)
+
+    @api.multi
+    def button_email(self):
+        print 'email'
+        raise ValidationError("Mark inserted email function here")
+
+    @api.multi
+    def button_cancel(self):
+        self.state = 'cancel'
+        self.processed_by = ''
+
+    config_ids = fields.One2many ('asset.config.inherit', 'config_id')
+
+
+class AssetConfigInherit (models.Model):
+    _name = 'asset.config.inherit'
+
+    config_id = fields.Many2one ('asset.management.handover')
+    asset_id = fields.Many2one('asset.config', string = "Asset", required = True)
+    serial_num = fields.Char(string = "Serial #", related = 'asset_id.serial_num', readonly = True)
+    model = fields.Char (string = "Model", related = 'asset_id.asset_model', readonly = True)
+    asset_year  = fields.Date(string = "Year", related = 'asset_id.year', readonly = True)
+    condition = fields.Selection (string = "Asset Condition", related = 'asset_id.asset_condition', readonly = True)
+    quantity = fields.Char (string = "Quantity", default = "1")
