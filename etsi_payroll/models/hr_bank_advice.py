527b78e950b7298773e7b94f49c724c4f0c9ea25
Mark Angelo S. Barnatcha <markangelobarnatcha014@gmail.com>
2017-09-07 09:01:14 +0800

    little changes

--- etsi_payroll/models/hr_bank_advice.py
+++ etsi_payroll/models/hr_bank_advice.py
@@ -109,7 +109,7 @@ class BankAdvice(models.Model):
             'view_type': 'form',
             'view_mode': 'form',
             'res_model': 'mail.compose.message',
-            'views': [(compose_form_id, 'form')],
+            'views': [(compose_form_id, 'form')] ,
             'view_id': compose_form_id,
             'target': 'new',
             'context': ctx,
