48a980df2bc492eb9d573434f0390af37d00c46d
elishah19 <elishah.garcia019@gmail.com>
2017-09-27 09:46:01 +0800

    etsi_payroll, Payslip batches_send by email

--- etsi_payroll/models/hr_payroll.py
+++ etsi_payroll/models/hr_payroll.py
@@ -1310,6 +1310,7 @@ class HrPayslipEmployees(models.TransientModel):
                 emp_ids.append(id.employee_id.id)
         result['emp_exclude_count'] = count
         result['employee_ids'] = emp_ids
+        print 'emp_idsss', emp_ids
         return result
 
 class Excluded_Employee(models.Model):
@@ -1324,6 +1325,46 @@ class Hr_Payslip_Run(models.Model):
     ex_emp_count = fields.Integer(compute ='_compute_emp_count', string="Payslip not generated")
     payslip_count = fields.Integer(compute='_compute_payslip_count', string="Payslip generated")
     exclude_emp_ids = fields.One2many('excluded.employee','payslip_run_id',string="Exempted Employee")
+
+    @api.multi
+    def send_payslip(self):
+        """
+        This function opens a window to compose an email, with the edi payslip template message loaded by default
+        """
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('send_email_payslips', 'email_template_hr_payslip')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+
+        print 'user', self.employee_id.user_id
+
+        user = self.env['res.users'].browse(self.employee_id.user_id.id)
+        print 'partner_id', user.partner_id.id
+        ctx = dict()
+        ctx.update({
+            'default_model': 'hr.payslip',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'default_partner_id': user.partner_id.id,
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
 
     @api.multi
     def _compute_payslip_count(self):
